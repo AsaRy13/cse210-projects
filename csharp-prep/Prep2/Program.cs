34d2741ccaa1dbabc1f319e39e47657e578bf37f
AsaRy13 <fle20007@byui.edu>
2024-04-25 21:39:48 -0600

    Updated the Program.cs file in the Prep2 directory to output a grade letter based on an inputted grade percentage.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage in your class?: ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter;
+        int remainder = gradePercentage % 10;
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (remainder >= 7 && letter != "A" && letter != "F")
+        {
+            letter = letter + "+";
+        }
+        else if (remainder < 3 && letter != "F" && gradePercentage < 100)
+        {
+            letter = letter + "-";
+        }
+
+        Console.WriteLine(letter);
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, but you did not pass. Just keep learning and eventually you will pass.");
+        }
     }
 }
