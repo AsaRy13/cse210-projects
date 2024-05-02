5dba182fad70ee8f68f6b792fda3e365ff79036a
AsaRy13 <fle20007@byui.edu>
2024-05-01 23:56:12 -0600

    Changed Program.cs to ask for the name of the user and their favorite number and to output their name and their favorite number squared.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string uName = PromptUserName();
+        int uNumber = PromptUserNumber();
+        int squared = SquareNumber(uNumber);
+        DisplayResult(uName, squared);
+
+    }
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName() {
+        Console.Write("What is your name?: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber() {
+        Console.Write("What is your favorite number?: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+    static int SquareNumber(int baseNumber) {
+        int squaredNumber = Convert.ToInt32(Math.Pow(baseNumber, 2));
+        return squaredNumber;
+    }
+    static void DisplayResult(string theName, int theSquared) {
+        Console.WriteLine($"{theName}, the square of your number is {theSquared}.");
     }
 }
