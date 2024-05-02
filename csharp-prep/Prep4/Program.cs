1b4172c056489b279fd6b21eabd22e4a70ec33ce
AsaRy13 <fle20007@byui.edu>
2024-05-01 23:14:39 -0600

    Changed to make it so the user can input a list of numbers and the program will output the sum, average, largest number, and smallest positive number of the list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int inputtedNumber = 0;
+        float numberSum = 0;
+        float numberAverage = 0;
+        int largestNumber = 0;
+        int smallestPositiveNumber = 0;
+
+        do {
+            Console.Write("Enter number: ");
+            inputtedNumber = int.Parse(Console.ReadLine());
+            numbers.Add(inputtedNumber);
+            if (inputtedNumber != 0) {
+                numberSum += inputtedNumber;
+                numberAverage = numberSum / numbers.Count();
+                if (inputtedNumber > largestNumber) {
+                    largestNumber = inputtedNumber;
+                }
+                if (smallestPositiveNumber == 0) {
+                    smallestPositiveNumber = largestNumber;
+                }
+                else if (inputtedNumber < smallestPositiveNumber && inputtedNumber > 0) {
+                    smallestPositiveNumber = inputtedNumber;
+                }
+            }
+            else {
+                numbers.Remove(0);
+            }
+        } while (inputtedNumber != 0);
+
+        Console.WriteLine($"The sum is: {numberSum}");
+        Console.WriteLine($"The average is: {numberAverage}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
     }
 }
