e8cd31b14be84d4b5d4571c8b65d33f72d41064b
AsaRy13 <167818887+AsaRy13@users.noreply.github.com>
2024-06-22 03:28:43 -0600

    Created everything for the ICE Smart Home App

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,12 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Account account = new Account();
+        int quit = 0;
+        while(quit == 0) {
+            Console.Clear();
+            Console.WriteLine("Sandbox Menu");
+            Console.WriteLine("1. ICE Smart Home");
+            Console.WriteLine("2. Test");
+            Console.WriteLine("3. Quit");
+            Console.Write("Please input the number of the option that you would like to select: ");
+            string userInput = Console.ReadLine();
 
-        Console.WriteLine($"You have ${account.GetAccountBalance()}");
-
-        account.Withdraw();
-
-        Console.WriteLine($"You have ${account.GetAccountBalance()}");
+            if(userInput == "1") {
+                ICESmartHome.SmartHome();
+            }
+            else if(userInput == "2") {
+                Test.RunTimeTest();
+            }
+            else if(userInput == "3") {
+                quit = 1;
+            }
+        }
     }
 }
